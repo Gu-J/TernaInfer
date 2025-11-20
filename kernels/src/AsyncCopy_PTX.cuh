edfa7e3e625ec0b3bbadd0d24cfb61eda8e3a4c9
Gu-J <416594418@qq.com>
2025-11-20 18:16:45 +0800

    1

--- kernels/src/AsyncCopy_PTX.cuh
+++ kernels/src/AsyncCopy_PTX.cuh
@@ -1,3 +1,17 @@
+/***************************************************************************
+ * Copyright 2025 The TernaInfer Authors. All rights reserved.
+ * Copyright 2025 The SpInfer Authors. All rights reserved.
+ * Copyright 2023 The FLash-LLM Authors. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
 template<int SizeInBytes>
 __device__ __forceinline__ void cp_async(half* smem_ptr, const half* global_ptr, bool pred_guard = true)
 {
